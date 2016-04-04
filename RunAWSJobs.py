ca75e9a5d0a7706977832fce9f5642e43ac8c82c
natetlawrence <nlaw85@gmail.com>
2016-04-04 11:53:41 -0700

    fix security group

--- RunAWSJobs.py
+++ RunAWSJobs.py
@@ -30,7 +30,7 @@ class instances(object):
         self.ImageId = 'ami-f7433e97'
         self.KeyName = 'KeyPair150805'
         self.InstanceType = 't1.micro'
-        self.SecurityGroupIds = ['sg-d0bcaeb5']
+        self.SecurityGroupIds = ['sg-9a4f8ffe']
         self.key = '~/.ssh/KeyPair150805.pem'
         self.username = 'ubuntu'
 
